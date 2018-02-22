2782c6408d27028be027fccef179ef3fe2408a8f
MazelTovCocktail <sean.peter.conrad@gmail.com>
2018-02-22 13:21:12 -0600

    fully working version
    -created separate function called tenthFrame() that handles special case for 10th frame.
    -changed checkInput() function into getInput() that now handles all inputs and input checking.
    -modified printScores() function to correctly calculated and display all scores.
    -fix a few bugs...

--- bowling.cpp
+++ bowling.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 const int tenthframe = 19;
-int checkInput(int ball, int score, int balls[]); //NEEDS WORK. gets input and checks if input is valid, loops until valid input
-void printScores(int b, int balls[]);
+int getInput(int ball, int balls[]); //gets input and checks if input is valid, loops until valid input
+void printScores(int ball, int balls[]);
 int getNextBalls(int i, int balls[]);//returns the value of the next 2 balls
+void tenthFrame(int balls[]);
 
 int main()
 {
@@ -21,9 +22,7 @@ int main()
 	while(ball < tenthframe)
 	{
 		score = 0;
-		cout<<"Next Ball: ";
-		cin>>score;
-		score = checkInput(ball, score, balls);//needs work
+		score = getInput(ball, balls);
 		balls[ball-1]=score;
 		if(ball % 2 != 0 && score == 10)//is strike...
 			++ball; //skip next ball
@@ -31,29 +30,48 @@ int main()
 		printScores(ball, balls);
 	}
 	
-	score = 0;
-	//begin special case for frame 10
+	tenthFrame(balls);
 	
 	return 0;
 }
 
-void printScores(int b, int balls[])
+void printScores(int ball, int balls[])
 {
 	int total=0;
-	for(int i=0; i<19; i=i+2)
+	
+	//print ball scores
+	for(int i=0; i<18; i=i+2)
+		cout<<"|"<<balls[i]<<" "<<balls[i+1];
+	cout<<"|"<<balls[18]<<" "<<balls[19]<<" "<<balls[20]<<"|"<<endl;
+	
+	//print frame totals
+	for(int i=0; i<ball-1; i=i+2)
 	{
 		if(balls[i]!=10) //if not strike...
 		{	//print normally
 			total += balls[i]+balls[i+1];
-			cout<<balls[i]<<" "<<balls[i+1]<<" Frame Total: "<<balls[i]+balls[i+1]<<endl;
+			cout<<"| "<<balls[i]+balls[i+1]<<" ";
 		}
 		else //is strike...
 		{	//print special
-			total += balls[i]+balls[i+1]+getNextBalls(i+2, balls);
-			cout<<balls[i]<<" "<<balls[i+1]<<" Frame Total: "<<(balls[i]+balls[i+1]+getNextBalls(i+2, balls))<<endl;
+			if(i!=16)//not in 9th frame
+			{
+				total += balls[i]+balls[i+1]+getNextBalls(i+2, balls);
+				cout<<"| "<<balls[i]+balls[i+1]+getNextBalls(i+2, balls)<<" ";
+			}
+			else//in 9th frame
+			{
+				total += balls[i]+balls[18]+balls[19];
+				cout<<"| "<<balls[i]+balls[18]+balls[19]<<" ";
+			}
 		}
 	}
-	cout<<"GRAND TOTAL: "<<total<<endl;
+	if(ball >= tenthframe)
+	{
+		total += balls[18]+balls[19]+balls[20];
+		cout<<"| "<<balls[18]+balls[19]+balls[20]<<"   ";
+	}
+	cout<<"|\nGRAND TOTAL: "<<total<<endl;
 }
 
 //returns value of next 2 balls
@@ -71,18 +89,104 @@ int getNextBalls(int i, int balls[])
 }
 	
 
-int checkInput(int ball, int score, int balls[])
+int getInput(int ball, int balls[])
 {
-	while((score < 0 || score > 10) || (ball%2 == 0 && (balls[ball-1] + score > 10)))
+	int score = 0;
+	bool valid = false;
+	bool notInt = false;
+	
+	cout<<"Next Ball: ";
+	cin>>score;
+	
+	if(cin.fail())//not an int type
 	{
-		cout<<"Invalid Input. Try again."<<endl;
-		cin>> score;
+		notInt = true;
+		cin.clear();
+	}
+		
+	while(notInt)
+	{
+		cin.clear();
+		cout<<"Invalid Input1. Try again:";
+		cin.ignore();
+		cin>>score;
+		if(cin.good())
+			notInt=false;
+	}
+	
+	if(ball < tenthframe)//not tenth frame
+	{
+		while( !valid )
+		{	
+			if( score >= 0 && score <=10 )// score is in range
+			{
+				if(ball%2 == 0 && balls[ball-2]+score <= 10)//on 2nd ball and ball1 + input <= 10
+					valid = true;
+				else if(ball%2 != 0)
+					valid = true;
+			}
+		
+			if( !valid )//input not valid
+			{
+				cout<<"Invalid Input2. Try again:";
+				cin>> score;
+			}
+		}
+	}
+	else //is tenth frame
+	{
+		while( !valid )
+		{
+			if( score >= 0 && score <= 10 )//score is in range
+			{
+				if( balls[18]+balls[19]+balls[20]+score <= 30)//10th frame total <= 30
+					valid = true;
+			}
+			
+			if( !valid )//invalid input
+			{
+				cout<<"Invalid Input3. Try again."<<endl;
+				cin>>score;
+			}
+		}
 	}
 	return score;
 }
 
 
+void tenthFrame(int balls[])
+{
+	int roll1, roll2, roll3; //3 rolls of the 10th frame
+	int total_score = 0; //total score for the 10th frame
+	int tenthTotal = 0;
+	
+	//get roll1
+	roll1 = getInput(tenthframe, balls);
+	balls[18]=roll1;
+	
+	//print scores
+	printScores(tenthframe, balls);
+	
+	//validate roll2
+	roll2 = getInput(tenthframe, balls);
+	balls[19]=roll2;
+	
+	//print scores
+	printScores(tenthframe, balls);
+	
+	//begin roll 3 if stike or spare
+	if(roll1+roll2 >= 10)
+	{
+		//get roll3
+		roll3 = getInput(tenthframe, balls);
+		balls[20]=roll3;
+		
+		//print scores
+		printScores(tenthframe, balls);
 
+	}
+	//end of game...
+}
 
 
 
